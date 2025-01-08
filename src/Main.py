689e8896bcbbe98b655a4ee8b9a3a926e2bbd340
Anthony Daves <adaves1@yahoo.com>
2025-01-08 15:00:45 +0530

    Create Main.py

---
+++ src/Main.py
@@ -0,0 +1,24 @@
+import sys
+
+dateYY = input("Enter the year (YYYY): ")
+if len(dateYY) != 4:
+  print(f"bro. u think we would believe you live in {dateYY}?")
+  sys.exit("Bro lied")
+else:
+  dateMM = input("Enter the month (MM): ")
+  if len(dateMM) != 2:
+    print(f"tf is month {dateMM} 🙏😭")
+    sys.exit("Bro lied")
+  else:
+    dateDD = input("Enter today's date (DD): ")
+    if len(dateDD) != 2:
+      print("bros living in pluto")
+      sys.exit("Bro lied")
+    else:
+      print("thinking some shit..."
+if dateMM = 1:
+  if dateDD = 9:
+    print("hbd anthony-san")
+
+sys.exit("prrepdhfwrhfyierwfiyryir")
+      
